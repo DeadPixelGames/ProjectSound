9e80f00e4fc6ae75a8aa9712d1c54e847aedb795
JuliaCajigal <43203288+JuliaCajigal@users.noreply.github.com>
2019-12-02 00:39:31 +0100

    Commit of all commits, main menu and loooooooots of animations.
    Tengo miedo.

---
+++ ProjectSound/Assets/MainMenu/Scripts/CATALOGO_handler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CATALOGO_handler : MonoBehaviour
+{
+
+    [SerializeField] private Animator catalogo_animator;
+
+
+    public void pasarPaginaDerecha()
+    {
+        catalogo_animator.SetBool("pagina1", true);
+        catalogo_animator.SetBool("pagina2", false);
+        Debug.Log("HOLA");
+    }
+
+    public void pasarPaginaIzquierda()
+    {
+        Debug.Log("Epaaaaaa");
+        catalogo_animator.SetBool("pagina1", false);
+        catalogo_animator.SetBool("pagina2", true);
+    }
+}
---
+++ ProjectSound/Assets/MainMenu/Scripts/CUADERNO_handler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CUADERNO_handler : MonoBehaviour
+{
+
+
+    [SerializeField] private Animator cuaderno_animator;
+
+    public void salir()
+    {
+        cuaderno_animator.SetBool("salir", true);
+        Debug.Log("Saliendo");
+    }
+
+}
