991ddc1188e4ef60f142266e94c4e56b2c734ab3
RaulArenas <43203292+RaulArenas@users.noreply.github.com>
2019-12-04 17:55:33 +0100

    Catalogue fixed

--- ProjectSound/Assets/MainMenu/Scripts/CATALOGO_handler.cs
+++ ProjectSound/Assets/MainMenu/Scripts/CATALOGO_handler.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CATALOGO_handler : MonoBehaviour
 {
 
     [SerializeField] private Animator catalogo_animator;
+    public Button Derecha;
+    public Button Izquierda;
+ 
 
+            private void Start()
+    {
+        Izquierda.gameObject.SetActive(false);
+        Derecha.gameObject.SetActive(true);
+    }
 
     public void pasarPaginaDerecha()
     {
+        Izquierda.gameObject.SetActive(true);
+        Derecha.gameObject.SetActive(false);
         catalogo_animator.SetBool("pagina1", true);
         catalogo_animator.SetBool("pagina2", false);
         Debug.Log("HOLA");
@@ -17,6 +28,8 @@ public class CATALOGO_handler : MonoBehaviour
 
     public void pasarPaginaIzquierda()
     {
+        Izquierda.gameObject.SetActive(false);
+        Derecha.gameObject.SetActive(true);
         Debug.Log("Epaaaaaa");
         catalogo_animator.SetBool("pagina1", false);
         catalogo_animator.SetBool("pagina2", true);
