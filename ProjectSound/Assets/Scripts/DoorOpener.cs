8395d2ef52f45b37e19514d1d2c8cf83bc8c6a2d
RaulArenas <43203292+RaulArenas@users.noreply.github.com>
2019-12-07 21:45:11 +0100

    Exit door + The End fixed

--- ProjectSound/Assets/Scripts/DoorOpener.cs
+++ ProjectSound/Assets/Scripts/DoorOpener.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class DoorOpener : MonoBehaviour, IZappable {
     
     public Animator door;
+    public GameObject luzRoja;
+    public GameObject luzVerde;
+    public GameObject luzPuertaRoja;
+    public GameObject luzPuertaVerde;
+
+    private void Start()
+    {
+        luzRoja.gameObject.SetActive(false);
+        luzVerde.gameObject.SetActive(true);
+        luzPuertaRoja.gameObject.SetActive(true);
+        luzPuertaVerde.gameObject.SetActive(false);
+    }
 
     public void Zap() {
         this.door.SetTrigger("Open");
+        luzRoja.gameObject.SetActive(true);
+        luzVerde.gameObject.SetActive(false);
+        luzPuertaRoja.gameObject.SetActive(false);
+        luzPuertaVerde.gameObject.SetActive(true);
     }
 
 }
